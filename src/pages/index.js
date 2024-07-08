98b9adaab9b9fa3d661c3af92d53edb688cfeee2
Javier Paez Franco <j.paezfranco@student.tudelft.nl>
2024-07-09 00:06:47 +0200

    Fixed mouse gradient

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,79 +1,68 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import { Layout, Hero, About, Jobs, Featured, Projects, Contact } from '@components';
 
 const StyledMainContainer = styled.main`
   counter-reset: section;
 `;
 
-// const GlobalStyle = createGlobalStyle`
-//   html body {
-//     height: 100vh;
-//     width: 100%;
-//     background-color: var(--navy) !important;
-//     background-image: radial-gradient(
-//       circle 200px at var(--x, 10px) var(--y, 10px),
-//       rgb(12, 28, 53) 1%,
-//       var(--navy) 200%
-//     ) !important;
-//   }
-// `;
+// Background color of our page
+const GlobalStyle = createGlobalStyle`
+  html body {
+    height: 100vh;
+    width: 100%;
+    background-color: var(--navy) !important;
+  }
+`;
 
-// const IndexPage = ({ location }) => {
-//   useEffect(() => {
-//     let lastMousePosition = { x: 0, y: 0 };
-//
-//     const updateMousePosition = ev => {
-//       // Update last known mouse position
-//       lastMousePosition = { x: ev.pageX, y: ev.pageY };
-//
-//       // Use pageX and pageY from the last known mouse position, adjusted for current scroll position
-//       const x = lastMousePosition.x - document.body.scrollLeft;
-//       const y = lastMousePosition.y - document.body.scrollTop;
-//
-//       document.documentElement.style.setProperty('--x', `${x}px`);
-//       document.documentElement.style.setProperty('--y', `${y}px`);
-//     };
-//
-//     window.addEventListener('mousemove', updateMousePosition);
-//     window.addEventListener('scroll', updateMousePosition);
-//
-//     return () => {
-//       window.removeEventListener('mousemove', updateMousePosition);
-//       window.removeEventListener('scroll', updateMousePosition);
-//     };
-//   }, []);
-//
-//   return (
-//     <div>
-//       <GlobalStyle/>
-//       <Layout location={location}>
-//         <StyledMainContainer className="fillHeight">
-//           <Hero/>
-//           <About/>
-//           <Jobs/>
-//           <Featured/>
-//           <Projects/>
-//           <Contact/>
-//         </StyledMainContainer>
-//       </Layout>
-//     </div>
-//   );
-// };
+// Gradient that follows the mouse, based on: https://codepen.io/Iseyaaaaa/pen/qBMNEGN
+const Gradient = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 650px;
+  height: 650px;
+  border-radius: 100%;
+  background-image: radial-gradient(#0e2955 1%, #0a192f 200%);
+  filter: blur(250px);
+  transition: all 450ms ease-out;
+  position: fixed;
+  pointer-events: none;
+  left: 0;
+  top: 0;
+  transform: translate(calc(-50% + 15px), -50%);
+  z-index: -1;
+`;
 
-const IndexPage = ({ location }) => (
-  <Layout location={location}>
-    <StyledMainContainer className="fillHeight">
-      <Hero />
-      <About />
-      <Jobs />
-      <Featured />
-      <Projects />
-      <Contact />
-    </StyledMainContainer>
-  </Layout>
-);
+const IndexPage = ({ location }) => {
+  // We animate the gradient so that it follows the mouse pointer
+  useEffect(() => {
+    const cursor = document.querySelector('#gradient');
+
+    document.addEventListener('mousemove', function(e) {
+      cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`;
+    });
+  }, []);
+
+  // finally, we render the page
+  return (
+    <div>
+      <GlobalStyle />
+      <Layout location={location}>
+        <Gradient id="gradient" />
+        <StyledMainContainer className="fillHeight">
+          <Hero />
+          <About />
+          <Jobs />
+          <Featured />
+          <Projects />
+          <Contact />
+        </StyledMainContainer>
+      </Layout>
+    </div>
+  );
+};
 
 IndexPage.propTypes = {
   location: PropTypes.object.isRequired,
