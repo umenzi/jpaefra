ba96bf3823cafea63f050de0ef767ff5d92d5391
Javier Paez Franco <j.paezfranco@student.tudelft.nl>
2024-07-08 21:19:00 +0200

    Added mouse gradient

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,66 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import { Layout, Hero, About, Jobs, Featured, Projects, Contact } from '@components';
 
 const StyledMainContainer = styled.main`
   counter-reset: section;
 `;
 
-const IndexPage = ({ location }) => (
-  <Layout location={location}>
-    <StyledMainContainer className="fillHeight">
-      <Hero />
-      <About />
-      <Jobs />
-      <Featured />
-      <Projects />
-      <Contact />
-    </StyledMainContainer>
-  </Layout>
-);
+const GlobalStyle = createGlobalStyle`
+  html body {
+    height: 100vh;
+    width: 100%;
+    background-color: var(--navy) !important;
+    background-image: radial-gradient(
+      circle 300px at var(--x, 10px) var(--y, 10px),
+      rgba(109, 109, 239, 0.16) 0%,
+      var(--navy) 100%
+    ) !important;
+  }
+`;
+
+const IndexPage = ({ location }) => {
+  useEffect(() => {
+    let lastMousePosition = { x: 0, y: 0 };
+
+    const updateMousePosition = ev => {
+      // Update last known mouse position
+      lastMousePosition = { x: ev.pageX, y: ev.pageY };
+
+      // Use pageX and pageY from the last known mouse position, adjusted for current scroll position
+      const x = lastMousePosition.x - document.body.scrollLeft;
+      const y = lastMousePosition.y - document.body.scrollTop;
+
+      document.documentElement.style.setProperty('--x', `${x}px`);
+      document.documentElement.style.setProperty('--y', `${y}px`);
+    };
+
+    window.addEventListener('mousemove', updateMousePosition);
+    window.addEventListener('scroll', updateMousePosition);
+
+    return () => {
+      window.removeEventListener('mousemove', updateMousePosition);
+      window.removeEventListener('scroll', updateMousePosition);
+    };
+  }, []);
+
+  return (
+    <div>
+      <GlobalStyle />
+      <Layout location={location}>
+        <StyledMainContainer className="fillHeight">
+          <Hero />
+          <About />
+          <Jobs />
+          <Featured />
+          <Projects />
+          <Contact />
+        </StyledMainContainer>
+      </Layout>
+    </div>
+  );
+};
 
 IndexPage.propTypes = {
   location: PropTypes.object.isRequired,
