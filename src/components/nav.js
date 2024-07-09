cd321e22645630e8c1aa48fefdc289151958b5e0
Javier Paez Franco <j.paezfranco@student.tudelft.nl>
2024-07-09 20:05:31 +0200

    Added alternative hover when resume button

--- src/components/nav.js
+++ src/components/nav.js
@@ -98,6 +98,12 @@ const StyledLinks = styled.div`
     ${({ theme }) => theme.mixins.smallButton};
     margin-left: 15px;
     font-size: var(--fz-xs);
+
+    &:hover,
+    &:focus {
+      cursor: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='40' height='48' viewport='0 0 100 100' style='fill:black;font-size:24px;'><text y='50%'>📜</text></svg>") 20 0,
+      auto;
+    }
   }
 `;
 
