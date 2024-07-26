a741040b2a948c21ec573d261b9f551b58039df8
Javier Paez Franco <j.paezfranco@student.tudelft.nl>
2024-07-26 16:30:16 +0200

    Removed Google Analytics. Site verification is kept for indexing purposes

--- gatsby-config.js
+++ gatsby-config.js
@@ -193,27 +193,6 @@ module.exports = {
         ],
       },
     },
-    {
-      // Google Analytics
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        trackingIds: ['GTM-WL7PS7HG'],
-      },
-      // This object gets passed directly to the gtag config command
-      // This config will be shared across all trackingIds
-      gtagConfig: {
-        // We care for the privacy of our users :)
-        anonymize_ip: true,
-        cookie_expires: 0,
-      },
-      // This object is used for configuration specific to this plugin
-      pluginConfig: {
-        // Puts tracking script in the head instead of the body
-        head: false,
-        // Setting this parameter is also optional
-        respectDNT: true,
-      },
-    },
   ],
 };
 
